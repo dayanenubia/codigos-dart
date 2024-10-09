24af630d3bbe1af79ad1743e5f2ee0e0f96c34bc
Dayane Nubia Moreira Ferreira <38954586864@campus.machado.mch>
2024-10-09 19:17:23 -0300

    Descrição das alterações

--- exemplo1.dart
+++ exemplo1.dart
@@ -1,3 +1,5 @@
-void main(){
-  print('Olá, Mundo!');
+void main() { 
+  // Função principal, ponto de entrada do programa.
+  print('Olá, Mundo!'); 
+  // Função que imprime o texto 'Olá, Mundo!' no console.
 }
